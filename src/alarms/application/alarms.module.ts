dbf3e64f2c148f055d0e7bca6c1843def24b429f
ittikorn chawkamud <ittikorn.chawkamud@scbtechx.io>
2024-04-14 01:16:26 +0700

    Add NotifyFacilitySupervisorCommand and NotifyFacilitySupervisorCommandHandler

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -1,11 +1,14 @@
 import { DynamicModule, Module, Type } from '@nestjs/common';
-import { AlarmsService } from './alarms.service';
-import { AlarmsController } from '../presenters/http/alarms.controller';
 import { AlarmFactory } from '../domain/factories/alarm.factory';
-import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
-import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
-import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
+import { AlarmsController } from '../presenters/http/alarms.controller';
+import { AlarmsService } from './alarms.service';
 import { AcknowledgeAlarmCommandHandler } from './commands/acknowledge-alarm.command-handler';
+import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
+import { NotifyFacilitySupervisorCommandHandler } from './commands/notify-facility-supervisor.command-handler';
+import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
+import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
+import { CascadingAlarmsSaga } from './sagas/cascading-alarms.saga';
+import { UnacknowledgedAlarmsSaga } from './sagas/unacknowledged-alarms.saga';
 
 @Module({
   controllers: [AlarmsController],
@@ -16,6 +19,9 @@ import { AcknowledgeAlarmCommandHandler } from './commands/acknowledge-alarm.com
     GetAlarmsQueryHandler,
     AlarmCreatedEventHandler,
     AcknowledgeAlarmCommandHandler,
+    NotifyFacilitySupervisorCommandHandler,
+    CascadingAlarmsSaga,
+    UnacknowledgedAlarmsSaga,
   ],
 })
 export class AlarmsModule {
